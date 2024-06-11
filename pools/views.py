1f319984cfd8bc794c179967058c4f8eafc39ca0
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-06-11 16:47:24 +0700

    Write views that actually do something

--- pools/views.py
+++ pools/views.py
@@ -1,7 +1,17 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 def index(request):
-	return HttpResponse("Hello world. You're at the pools index.")
+	template = loader.get_template("pools/index.html")
+
+	questions = Question.objects.order_by("-pub_date")[:5]
+	context = {
+		"questions": questions
+	}
+	response_content = template.render(context=context, request=request)
+	return HttpResponse(response_content)
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at question %s." % question_id)
