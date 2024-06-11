3e19f6380b4aacc4847c14c1c3fa46fbaf176dca
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-06-11 15:06:58 +0700

    Playing with the API

--- pools/models.py
+++ pools/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db.models import Model
 from django.db.models import CharField
 from django.db.models import DateTimeField
@@ -6,11 +8,22 @@ from django.db.models import IntegerField
 from django.db.models import ForeignKey
 from django.db.models import CASCADE
 
+from django.utils import timezone
+
 class Question(Model):
   question_text = CharField(max_length=20)
   pub_date = DateTimeField("date published")
+
+  def __str__(self):
+    return f"Question(id:{self.id})"
+
+  def was_published_recently(self):
+    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(Model):
   question = ForeignKey(Question, on_delete=CASCADE)
   choice_text = CharField(max_length=200)
   votes = IntegerField(default=0)
+
+  def __str__(self):
+    return f"Choice(id:{self.id})"
