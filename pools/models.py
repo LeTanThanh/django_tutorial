e3dd3d372e5a1af87614312e2582016f404d4202
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-06-11 14:02:54 +0700

    Creating models

--- pools/models.py
+++ pools/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.db.models import Model
+from django.db.models import CharField
+from django.db.models import DateTimeField
+from django.db.models import CharField
+from django.db.models import IntegerField
+from django.db.models import ForeignKey
+from django.db.models import CASCADE
 
-# Create your models here.
+class Question(Model):
+  question_text = CharField(max_length=20)
+  pub_date = DateTimeField("date published")
+
+class Choice(Model):
+  question = ForeignKey(Question, on_delete=CASCADE)
+  choice_text = CharField(max_length=200)
+  votes = IntegerField(default=0)
