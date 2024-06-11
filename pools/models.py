f56942d3d736164c66879ce6a2b30fe3aa948a0c
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-06-11 16:24:24 +0700

    Writing more view

--- pools/models.py
+++ pools/models.py
@@ -3,27 +3,28 @@ import datetime
 from django.db.models import Model
 from django.db.models import CharField
 from django.db.models import DateTimeField
-from django.db.models import CharField
 from django.db.models import IntegerField
 from django.db.models import ForeignKey
 from django.db.models import CASCADE
 
 from django.utils import timezone
 
+
 class Question(Model):
-  question_text = CharField(max_length=20)
-  pub_date = DateTimeField("date published")
+	question_text = CharField(max_length=20)
+	pub_date = DateTimeField("date published")
 
-  def __str__(self):
-    return f"Question(id:{self.id})"
+	def __str__(self):
+		return f"Question(id:{self.id})"
 
-  def was_published_recently(self):
-    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(Model):
-  question = ForeignKey(Question, on_delete=CASCADE)
-  choice_text = CharField(max_length=200)
-  votes = IntegerField(default=0)
+	question = ForeignKey(Question, on_delete=CASCADE)
+	choice_text = CharField(max_length=200)
+	votes = IntegerField(default=0)
 
-  def __str__(self):
-    return f"Choice(id:{self.id})"
+	def __str__(self):
+		return f"Choice(id:{self.id})"
