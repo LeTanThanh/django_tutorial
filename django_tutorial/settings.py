01a5be366360ad3eeb9834b1369c7509ba2ac11d
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-06-11 14:15:23 +0700

    Activating models

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "pools.apps.PoolsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
