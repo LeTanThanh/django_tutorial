2cbe58a74d4153698226062780fa747c3fd6a506
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-06-17 12:43:08 +0700

    Write a minimal form

--- pools/urls.py
+++ pools/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = "pools"
 urlpatterns = [
   path("", views.index, name="questions"),
-  path("<int:question_id>/", views.detail, name="question_detail"),
-  path("<int:question_id>/results/", views.results, name="question_results"),
-  path("<int:question_id>/votes", views.vote, name="question_vote")
+  path("<int:question_id>", views.detail, name="question_detail"),
+  path("<int:question_id>/results", views.results, name="question_results"),
+  path("<int:question_id>/vote", views.vote, name="question_vote"),
+  path("<int:question_id>/detail", views.detail, name="question_detail")
 ]
--- pools/views.py
+++ pools/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
-from django.template import loader
 from django.http import Http404
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+from django.template import loader
+from django.urls import reverse
+from django.db.models import F
+
 from .models import Question
 
 def index(request):
@@ -29,8 +34,29 @@ def detail(request, question_id):
 	return HttpResponse(response_content)
 
 def results(request, question_id):
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
+	question = get_object_or_404(Question, id=question_id)
+	return render(
+     	request=request,
+     	template_name="pools/results.html",
+      	context={
+			"question": question
+		})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+	question = get_object_or_404(Question, id=question_id)
+	try:
+		selected_choice = question.choice_set.get(id=request.POST["choice"])
+	except ():
+		return render(
+			request = request,
+			template_name = "pools/detail.html",
+			context = {
+				"question": question,
+				"error_message": "You didn't select a choice."
+			}
+		)
+	else:
+		selected_choice.votes = F("votes") + 1
+		selected_choice.save()
+
+		return HttpResponseRedirect(reverse("pools:question_results", args=(question.id,)))
